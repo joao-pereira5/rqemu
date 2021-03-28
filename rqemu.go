c167d5d0f1b479799e6bb96c8a8b7c7602da43e4
john <john@bolorei.xyz>
2021-03-28 13:13:07 +0100

    rqemu GO lang file.
    Barebones help command and locate command.
    Help message will only print the main global help message (i.e. the one
    listing the commands and synopsis).
    The directory where rqemu will place VM config files and disks is
    located via XDG_HOME env variable (if available) or RQEMU_HOME.
    Locate command will print the directory in use by rqemu.

---
+++ rqemu.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//
+// utils
+func PrintErr(m interface{}) {
+	fmt.Fprintln(os.Stderr, m)
+}
+
+
+//
+// config variables
+var home string
+
+// environment variables
+const evarHome = "RQEMU_HOME"
+const evarXdgDataHome = "XDG_DATA_HOME"
+const evarXdgRuntimeDir = "XDG_RUNTIME_DIR"
+const evarXdgConfigHome = "XDG_CONFIG_HOME"
+
+
+//
+// help command
+func Help() {
+	msg := `NAME
+	rqemu - interactive command line QEMU user interface
+
+SYNOPSIS
+	rqemu <command> [<sub command>...]
+	rqemu help <command>
+
+COMMANDS
+	ls - list active VMs
+	lsall - list all VMs (active or inactive)
+	create
+	edit
+	start
+	help
+	stop
+	locate - print configuration directory
+	monitor - connect to a VM's QEMU monitor
+	spice - connect to a VM's SPICE server`
+
+	fmt.Println(msg)
+}
+
+func SetHomeDirectory() {
+	home = os.Getenv(evarHome)
+	if len(home) > 0 {
+		return
+	}
+	const dirName = "rqemu"
+
+	home = os.Getenv(evarXdgDataHome)
+	if len(home) > 0 {
+		home = home + "/" + dirName
+		return
+	}
+
+	home = os.Getenv(evarXdgRuntimeDir)
+	if len(home) > 0 {
+		home = home + "/" + dirName
+		return
+	}
+
+	home = os.Getenv(evarXdgConfigHome)
+	if len(home) > 0 {
+		home = home + "/" + dirName
+		return
+	}
+
+	home = os.Getenv("HOME")
+	if len(home) > 0 {
+		PrintErr("Couldn't find " +
+			evarHome + ", " +
+			evarXdgDataHome + ", " +
+			evarXdgRuntimeDir + ", " +
+			evarXdgConfigHome + "env variables, using HOME.")
+		home = home + "/." + dirName
+		return
+	}
+
+	PrintErr("Couldn't find " +
+		evarHome + ", " +
+		evarXdgDataHome + ", " +
+		evarXdgRuntimeDir + ", " +
+		evarXdgConfigHome + " or even HOME env variables, using PWD.")
+	home = dirName
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		Help()
+		os.Exit(1)
+	}
+
+	SetHomeDirectory()
+
+	switch args[0] {
+	case "help":
+		Help()
+
+	case "locate":
+		fmt.Println(home)
+
+	default:
+		PrintErr("No such command '" + args[0] + "'")
+		Help()
+		os.Exit(1)
+	}
+}
+
